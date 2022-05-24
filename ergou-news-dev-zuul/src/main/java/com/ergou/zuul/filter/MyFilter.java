7b8185cfd35d36fea2872cc905e8d462cf44f8ab
anqichen <anqi.chen@thoughtworks.com>
2022-05-24 23:06:25 +0800

    [anqichen] feat: dynamic config with git

--- ergou-news-dev-zuul/src/main/java/com/ergou/zuul/filter/MyFilter.java
+++ ergou-news-dev-zuul/src/main/java/com/ergou/zuul/filter/MyFilter.java
@@ -2,10 +2,20 @@ package com.ergou.zuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class MyFilter extends ZuulFilter {
+    @Value("${blackIp.continueCounts}")
+    private Integer continueCounts;
+    @Value("${blackIp.limitTimes}")
+    private Integer limitTimes;
+    @Value("${blackIp.timeInterval}")
+    private Integer timeInterval;
+
     @Override
     public String filterType() {
         return "pre";
@@ -24,6 +34,9 @@ public class MyFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         System.out.println("my filter ~~~");
+        System.out.println("continueCounts: " + continueCounts);
+        System.out.println("limitTimes: " + limitTimes);
+        System.out.println("timeInterval: " + timeInterval);
         return null;
     }
 }
--- spring-cloud-config/src/main/java/com/ergou/config/config.java
+++ spring-cloud-config/src/main/java/com/ergou/config/config.java
@@ -3,10 +3,12 @@ package com.ergou.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.ergou"}, exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
+@EnableConfigServer
 public class config {
 
     public static void main(String[] args) {
